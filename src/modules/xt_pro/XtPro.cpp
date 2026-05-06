22a38fdcbe2854163bea3c2297fa592f2626efac
KroseXina <yichaoguo1142@163.com>
2026-05-06 16:53:10 +0800

    init xt_pro

---
+++ src/modules/xt_pro/XtPro.cpp
@@ -0,0 +1,345 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2012-2019 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+ /**
+ * @file XtPro.cpp
+ *
+ * @brief 投喂机飞控自动规划航线，执行舵机PWM输出控制
+ *        与机载计算机端通信，获取目标航点信息，获取视觉降落辅助信息
+ * 	  需要通信的msg内容：本模块与mission模块，使用loiter_time，更改time逻辑为重量触发，重量数据从keyvalue获取
+ *			  xt_main_in:bool是否到达,float需要投放的重量
+ *			  xt_main_out:float当前料重,bool是否投料完成,bool是否开始进行降落（控制相机打开）,进入offboard
+ *  	  与机载机通信，需要添加到uxrce
+ *
+ * @version v1.0
+ *
+ * @author 巡天科技
+ */
+
+#include "XtPro.hpp"
+
+ModuleBase::Descriptor XtPro::desc{task_spawn, custom_command, print_usage};
+
+XtPro::XtPro() : ModuleParams(nullptr)
+{
+	PX4_INFO("Hello XT!");
+}
+
+XtPro::~XtPro()
+{
+
+}
+
+void XtPro::run()
+{
+	_xt_out.current_weight = 0.0f;
+	_xt_out.put_finish = false;
+	_xt_out.switch_to_offboard = false;
+
+	while (!should_exit())
+	{
+		//维持xt_main_out的发布
+		_xt_out.timestamp = hrt_absolute_time();
+		_xt_out_pub.publish(_xt_out);
+
+		//更新transponder report,并维护_targets列表
+		if(_transponder_report_sub.updated())
+		{
+			transponder_report_s msg{};
+			bool found = false;
+			_transponder_report_sub.copy(&msg);
+			if(msg.icao_address == 0) continue;
+			for(int i=0;i<_target_count;++i)
+			{
+				if(_targets[i].icao_address == msg.icao_address)
+				{
+					_targets[i] = msg;
+					found = true;
+					break;
+				}
+			}
+
+			if(!found && _target_count < MAX_TARGET)
+			{
+				_targets[_target_count++] = msg;
+			}
+			//为_targets排序
+			for (int i = 0; i < _target_count - 1; ++i)
+			{
+				for (int j = i + 1; j < _target_count; ++j)
+				{
+					if (_targets[j].icao_address < _targets[i].icao_address)
+					{
+						auto tmp = _targets[i];
+						_targets[i] = _targets[j];
+						_targets[j] = tmp;
+					}
+				}
+			}
+		}
+
+		_vehicle_status_sub.update(&_vehicle_status);
+		_mission_result_sub.update(&_mission_result);
+		_global_pos_sub.update(&_global_pos);
+
+		//监听mission是否执行到最后一个航点
+		if(_vehicle_status.nav_state == vehicle_status_s::NAVIGATION_STATE_AUTO_MISSION)
+			_vehicle_in_mission = true;
+
+		if(_mission_result_sub.updated())
+		{
+			if(_vehicle_in_mission && _mission_result.finished)
+			{
+				_vehicle_in_mission = false;
+				_xt_out.switch_to_offboard = true;
+			}
+		}
+
+		if(_vehicle_status.arming_state == vehicle_status_s::ARMING_STATE_DISARMED)
+			_xt_out.switch_to_offboard = false;
+
+		//生成航线,只有在系统就绪并且未解锁时可以执行
+		if(_vehicle_status.arming_state == vehicle_status_s::ARMING_STATE_DISARMED && _global_pos.lat_lon_valid)
+		{
+#ifdef __PX4_POSIX
+			//虚拟环境,启动后生成一次航线
+			if(!_mission_created)
+			{
+				create_mission();
+				PX4_INFO("target count %d",_target_count);
+				_mission_created = true;
+			}
+#else
+			//真实飞机使用遥控器指令生成航线
+			bool rc_trigger_now = false;
+			if(_input_rc_sub.update(&_input_rc))
+			{
+				const int channel = 7; //使用遥控器第8通道
+				if(channel < _input_rc.channel_count)
+					rc_trigger_now = (_input_rc.values[channel] > 1700);
+				//边沿触发
+				if(rc_trigger_now && !_xt_mission_valid)
+					create_mission();
+
+				_xt_mission_valid = rc_trigger_now;
+			}
+#endif
+		}
+
+		px4_usleep(200000);  //最高5Hz执行
+	}
+}
+
+
+void XtPro::create_mission()
+{
+	//1.清理旧的mission
+	mission_s mission_old{};
+	_dataman_client.readSync(DM_KEY_MISSION_STATE,
+				0, reinterpret_cast<uint8_t *>(&mission_old),
+				sizeof(mission_s));
+
+	mission_item_s empty_item{};
+	for(int i=0;i<math::max(mission_old.count,(uint16_t)(_target_count + 2));i++)
+	{
+		_dataman_client.writeSync(
+			DM_KEY_WAYPOINTS_OFFBOARD_0,
+			i,
+			reinterpret_cast<uint8_t *>(&empty_item),
+			sizeof(mission_item_s)
+			);
+	}
+
+	//2.设置当前位置为起飞点
+	mission_item_s takeoff{};
+	takeoff.nav_cmd = NAV_CMD_TAKEOFF;
+	takeoff.frame = NAV_FRAME_GLOBAL_RELATIVE_ALT;
+
+	takeoff.lat = _global_pos.lat;
+	takeoff.lon = _global_pos.lon;
+	takeoff.yaw = NAN;
+	takeoff.altitude = 5.0f;
+	takeoff.altitude_is_relative = true;
+
+	takeoff.autocontinue = true;
+	takeoff.acceptance_radius = 0.5f;
+
+	_dataman_client.writeSync(
+		DM_KEY_WAYPOINTS_OFFBOARD_0,
+		0,
+		reinterpret_cast<uint8_t *>(&takeoff),
+		sizeof(mission_item_s)
+		);
+
+	uint16_t wp_index = 1;
+	if(_target_count > 0)
+	{
+		//3.将targets航点写入dataman
+		for (int i = 0; i < _target_count; ++i)
+		{
+			mission_item_s mission_item{};
+			mission_item.nav_cmd = NAV_CMD_LOITER_TIME_LIMIT;
+			mission_item.frame = NAV_FRAME_GLOBAL_RELATIVE_ALT;
+
+			mission_item.lat = _targets[i].lat;
+			mission_item.lon = _targets[i].lon;
+			mission_item.yaw = NAN;
+			mission_item.altitude = 5.0f;
+			mission_item.altitude_is_relative = true;
+
+			mission_item.time_inside = 5.0f;
+
+			mission_item.autocontinue = true;
+			mission_item.acceptance_radius = 0.5f;
+
+			_dataman_client.writeSync(
+				DM_KEY_WAYPOINTS_OFFBOARD_0,
+				wp_index ++,
+				reinterpret_cast<uint8_t *>(&mission_item),
+				sizeof(mission_item_s)
+				);
+		}
+
+		//4.将当前位置设置为最后一个航点
+		mission_item_s home{};
+		home.nav_cmd = NAV_CMD_WAYPOINT;
+		home.frame = NAV_FRAME_GLOBAL_RELATIVE_ALT;
+
+		home.lat = _global_pos.lat;
+		home.lon = _global_pos.lon;
+		home.yaw = NAN;
+		home.altitude = 5.0f;
+		home.altitude_is_relative = true;
+
+		home.autocontinue = true;
+		home.acceptance_radius = 0.5f;
+
+		_dataman_client.writeSync(
+			DM_KEY_WAYPOINTS_OFFBOARD_0,
+			wp_index ++,
+			reinterpret_cast<uint8_t *>(&home),
+			sizeof(mission_item_s)
+			);
+	}
+
+	//5.生成整体mission信息，写入dataman，发布话题触发navigator更新
+	mission_s mission{};
+	mission.timestamp = hrt_absolute_time();
+
+	mission.mission_dataman_id = DM_KEY_WAYPOINTS_OFFBOARD_0;
+	mission.fence_dataman_id   = DM_KEY_FENCE_POINTS_0;
+	mission.safepoint_dataman_id = DM_KEY_SAFE_POINTS_0;
+
+	mission.count = wp_index;
+	mission.current_seq = -1;
+	mission.land_start_index = -1;
+	mission.land_index = -1;
+	mission.mission_id = mission_old.mission_id + 1;
+	mission.geofence_id = 0;
+	mission.safe_points_id = 0;
+
+	if(!_dataman_client.writeSync(
+		DM_KEY_MISSION_STATE,
+		0,
+		reinterpret_cast<uint8_t *>(&mission),
+		sizeof(mission_s)))
+	{
+		PX4_ERR("Mission state write failed");
+		return;
+	}
+
+	_mission_pub.publish(mission);
+}
+
+int XtPro::run_trampoline(int argc, char *argv[])
+{
+	return ModuleBase::run_trampoline_impl(desc, [](int ac, char *av[]) -> ModuleBase * {
+		return XtPro::instantiate(ac, av);
+	}, argc, argv);
+}
+
+int XtPro::task_spawn(int argc, char *argv[])
+{
+	desc.task_id = px4_task_spawn_cmd("xtpro",
+					  SCHED_DEFAULT,
+					  SCHED_PRIORITY_NAVIGATION,
+					  PX4_STACK_ADJUSTED(3000),
+					  (px4_main_t)&run_trampoline,
+					  (char *const *)argv);
+
+	if (desc.task_id < 0) {
+		desc.task_id = -1;
+		return -errno;
+	}
+
+	return 0;
+}
+
+XtPro *XtPro::instantiate(int argc, char *argv[])
+{
+	XtPro *instance = new XtPro();
+
+	if (instance == nullptr) {
+		PX4_ERR("alloc failed");
+	}
+
+	return instance;
+}
+
+int XtPro::custom_command(int argc, char *argv[])
+{
+	return print_usage("unknown command");
+}
+
+int XtPro::print_usage(const char *reason)
+{
+	if (reason) {
+		PX4_WARN("%s\n", reason);
+	}
+
+	PRINT_MODULE_DESCRIPTION(
+		R"DESCR_STR(
+### Description
+
+)DESCR_STR");
+
+	PRINT_MODULE_USAGE_COMMAND("start");
+	PRINT_MODULE_USAGE_NAME("xt_pro", "controller");
+	PRINT_MODULE_USAGE_DEFAULT_COMMANDS();
+	return 0;
+}
+
+extern "C" __EXPORT int xt_pro_main(int argc, char *argv[])
+{
+	return ModuleBase::main(XtPro::desc, argc, argv);
+}
---
+++ src/modules/xt_pro/XtPro.hpp
@@ -0,0 +1,125 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2020-2021 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include <px4_platform_common/px4_config.h>
+#include <px4_platform_common/defines.h>
+#include <px4_platform_common/module.h>
+#include <px4_platform_common/module_params.h>
+#include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
+#include <lib/mathlib/mathlib.h>
+#include <drivers/drv_hrt.h>
+#include <dataman_client/DatamanClient.hpp>
+#include <navigator/navigation.h>
+#include <containers/Array.hpp>
+
+#include <uORB/uORB.h>
+#include <uORB/Publication.hpp>
+#include <uORB/Subscription.hpp>
+#include <uORB/SubscriptionCallback.hpp>
+#include <uORB/topics/transponder_report.h>
+#include <uORB/topics/input_rc.h>
+#include <uORB/topics/mission.h>
+#include <uORB/topics/mission_result.h>
+#include <uORB/topics/vehicle_global_position.h>
+#include <uORB/topics/vehicle_status.h>
+#include <uORB/topics/xt_main_in.h>
+#include <uORB/topics/xt_main_out.h>
+
+using namespace time_literals;
+
+class XtPro : public ModuleBase, public ModuleParams
+{
+public:
+	static Descriptor desc;
+
+	XtPro();
+	~XtPro() override;
+
+	//PX4启动模块固定需要
+	/** @see ModuleBase */
+	static int task_spawn(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int run_trampoline(int argc, char *argv[]);
+
+	/** @see ModuleBase 在实时参与控制解算的系统中不调用*/
+	static XtPro *instantiate(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int custom_command(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int print_usage(const char *reason = nullptr);
+
+	void run() override;
+
+private:
+
+	uORB::Publication<mission_s>      _mission_pub{ORB_ID(mission)};
+	uORB::Publication<xt_main_out_s>  _xt_out_pub{ORB_ID(xt_main_out)};
+
+	uORB::Subscription                _transponder_report_sub{ORB_ID(transponder_report)};
+	uORB::Subscription                _input_rc_sub{ORB_ID(input_rc)};
+	uORB::Subscription                _global_pos_sub{ORB_ID(vehicle_global_position)};
+	uORB::Subscription                _mission_result_sub{ORB_ID(mission_result)};
+	uORB::Subscription                _vehicle_status_sub{ORB_ID(vehicle_status)};
+	uORB::Subscription                _xt_in_sub{ORB_ID(xt_main_in)};
+
+	input_rc_s                        _input_rc;
+	vehicle_global_position_s         _global_pos;
+	mission_result_s                  _mission_result;
+	vehicle_status_s                  _vehicle_status;
+	xt_main_in_s                      _xt_in;
+	xt_main_out_s                     _xt_out;
+
+	/* 用以生成航点 */
+	DatamanClient _dataman_client{};
+	static constexpr int MAX_TARGET = 50;
+	px4::Array<transponder_report_s,MAX_TARGET> _targets{};
+	int _target_count{0};
+	bool _xt_mission_valid{false};
+	int _trig_count{0};
+
+	bool _vehicle_in_mission{false};
+
+	//根据现有的target，生成任务
+	void create_mission();
+
+	//虚拟环境下使用变量
+#ifdef __PX4_POSIX
+	bool _mission_created{false};
+#endif
+
+};
